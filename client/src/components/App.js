6e2fc5537d21fcbd1fc8ddb4bfc2a811061c7fe3
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-09-05 22:26:14 +0100

    Intial setup for making a WhatsApp clone learned from https://www.youtube.com/watch?v=tBr-PybP_9c

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1>
+      WhatsApp clone with React & Socket.io learned from
+      https://www.youtube.com/watch?v=tBr-PybP_9c
+    </h1>
+  );
+}
+
+export default App;
