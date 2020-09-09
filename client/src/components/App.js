52c4eb984e499445bb1d0559db6f30aa505770a2
mark <markpackham1@gmail.com>
2020-09-09 16:53:24 +0100

    adding conversations, https://youtu.be/tBr-PybP_9c?t=3487

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,16 @@ import useLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id} />
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
   );
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 
 export default function Dashboard({ id }) {
   return (
-    <div className="d-flex" style={{ height: "100vh" }}>
+    <div className="d-flex" style={{ height: "80vh" }}>
       <Sidebar id={id} />
     </div>
   );
