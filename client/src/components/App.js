8395ecd719126ac59f991742f0b9afcf2ea5e7ef
mark <markpackham1@gmail.com>
2020-09-09 10:12:17 +0100

    finished off login page, https://youtu.be/tBr-PybP_9c?t=894

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Login from "./Login";
 
 function App() {
+  const [id, setId] = useState();
+
   return (
-    <h1>
-      WhatsApp clone with React & Socket.io learned from
-      https://www.youtube.com/watch?v=tBr-PybP_9c
-    </h1>
+    <>
+      <h1>
+        WhatsApp clone with React & Socket.io learned from
+        https://www.youtube.com/watch?v=tBr-PybP_9c
+      </h1>
+      <Login onIdSubmit={setId} />
+    </>
   );
 }
 
